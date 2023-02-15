c68929bfa4483c10f298cec2ba3ffbcfd3042bad
NathanWindeler <89609133+NathanWindeler@users.noreply.github.com>
2023-02-15 15:29:49 -0600

    Create sum function

---
+++ Assignment.java
@@ -0,0 +1,16 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public double sum(int[] nums) {
+		double sum = 0;
+		for(double num : nums)
+			sum += num;
+		return sum;
+	}
+
+}
