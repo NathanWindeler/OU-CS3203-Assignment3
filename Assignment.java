a5a87c5ea764afbb6117413a212a354e05589363
NathanWindeler <89609133+NathanWindeler@users.noreply.github.com>
2023-02-15 15:43:47 -0600

    Part 3: Add main method

--- Assignment.java
+++ Assignment.java
@@ -1,19 +1,32 @@
 
+import java.util.Scanner;
+
 public class Assignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Be careful when entering values - there's no error handling"); // I'm lazy lol
+		System.out.println("Enter size of array (integer): ");
+		int arrSize = kb.nextInt();
+		double[] nums = new double[arrSize];
+		System.out.println("Enter array contents (double): ");
+		for(int i = 0; i < arrSize; i++)
+			nums[i] = kb.nextDouble();
+		double sum = add(nums);
+		double product = multiply(nums);
+		System.out.printf("The sum is: %f \n", sum);
+		System.out.printf("The product is: %f \n", product);
 	}
 	
-	public double add(double[] nums) {
+	public static double add(double[] nums) {
 		double sum = 0;
 		for(double num : nums)
 			sum += num;
 		return sum;
 	}
 	
-	public double multiply(double[] nums) {
+	public static double multiply(double[] nums) {
 		double product = 1;
 		for(double num : nums)
 			product *= num;
