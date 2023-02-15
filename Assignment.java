04d6e760a2555c33463def6eb1bd932aacf45801
NathanWindeler <Nathan.M.Windeler-1@ou.edu>
2023-02-15 16:19:08 -0600

    Part10: Add some error handling

--- Assignment.java
+++ Assignment.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Assignment {
@@ -6,17 +7,26 @@ public class Assignment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Be careful when entering values - there's no error handling"); // I'm lazy lol
 		System.out.println("Enter size of array (integer): ");
 		int arrSize = kb.nextInt();
 		double[] nums = new double[arrSize];
 		System.out.println("Enter array contents (double): ");
-		for(int i = 0; i < arrSize; i++)
-			nums[i] = kb.nextDouble();
+		for(int i = 0; i < arrSize; i++) {
+			if(kb.hasNextDouble())
+				nums[i] = kb.nextDouble();
+			else {
+				kb.next();
+				i--;
+			}
+		}
 		double sum = add(nums);
 		double product = multiply(nums);
+		double[] reversed = reverse(nums);
+		System.out.println("Your array: " + Arrays.toString(nums));
 		System.out.printf("The sum is: %f \n", sum);
 		System.out.printf("The product is: %f \n", product);
+		System.out.println("Reversed array: " + Arrays.toString(reversed));
+		kb.close();
 	}
 	
 	public static double add(double[] nums) {
@@ -33,4 +43,11 @@ public class Assignment {
 		return product;
 	}
 
+	public static double[] reverse(double[] ray) {
+		double[] returnRay = new double[ray.length];
+		for(int i = 0; i < ray.length; i++) {
+			returnRay[ray.length-1-i] = ray[i];
+		}
+		return returnRay;
+	}
 }
